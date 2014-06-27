2499d88a24201f4472f133aad6768ff39935287c
Matt Basta <me@mattbasta.com>
2014-06-27 07:48:17 -0700

    Move IN to the some column

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var data = {
     "legal_ss": ['WA', 'IA', 'NY', 'VT', 'NH', 'CT', 'MA', 'MD', 'ME', 'RI', 'DE', 'MN', 'CA', 'NJ', 'HI', 'IL',
                  'NM', 'OR', 'PA', 'WI'],
     "legal_cu": ['NV', 'CO'],
-    "legal_some": ['TX', 'OK', 'AR', 'VA', 'ID', 'UT', 'MI-', 'SP-'],  // Michigan is weird.
+    "legal_some": ['TX', 'OK', 'AR', 'VA', 'ID', 'UT', 'IN', 'MI-', 'SP-'],  // Michigan is weird.
     "banned_const": ['AZ', 'NE', 'MS', 'TN', 'MT', 'AK', 'MO'],
-    "banned_leg": ['IN', 'WV', 'WY'],
+    "banned_leg": ['WV', 'WY'],
     "banned_both": ['NC', 'KS', 'SD', 'ND', 'LA', 'AL', 'FL', 'GA', 'SC', 'KY', 'OH']
 };
 
