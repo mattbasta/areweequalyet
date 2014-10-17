6d1c1b588a30052045677343753445382583c7b0
Matt Basta <mattbasta+github@gmail.com>
2014-10-17 10:33:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var data = {
     "legal_ss": ['WA', 'IA', 'NY', 'VT', 'NH', 'CT', 'MA', 'MD', 'ME', 'RI', 'DE', 'MN', 'CA', 'NJ', 'HI', 'IL',
-                 'NM', 'OR', 'PA', 'DC', 'IN', 'OK', 'UT', 'VA', 'WI', 'NV'],
+                 'NM', 'OR', 'PA', 'DC', 'IN', 'OK', 'UT', 'VA', 'WI', 'NV', 'AZ'],
     "legal_cu": ['CO'],
     "legal_some": ['TX', 'AR', 'KY', 'KS', 'AK', 'OH', 'WY', 'ID', 'MI-', 'SP-'],  // Michigan is weird.
-    "banned_const": ['AZ', 'NE', 'MS', 'TN', 'MT', 'MO'],
+    "banned_const": ['NE', 'MS', 'TN', 'MT', 'MO'],
     "banned_leg": ['WV'],
     "banned_both": ['NC', 'SD', 'ND', 'LA', 'AL', 'FL', 'GA', 'SC']
 };
